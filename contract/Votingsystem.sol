f53d8de09a5435383f3572da60b09ededbd1cc9a
Saba-sa <Sabaali3201@gmail.com>
2024-08-28 12:11:42 +0500

    voting system created

---
+++ contract/Votingsystem.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract VotingSystem {
+    mapping(address => bool) internal alreadyVoted;
+    
+    struct Candidate {
+        string name;
+        uint voteCount;
+    }
+
+    modifier hasVoted() {
+        require(!alreadyVoted[msg.sender], "You have already voted.");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, "You have no authority");
+        _;
+    }
+
+    mapping(uint => Candidate) internal candidates;
+    uint public candidatesCount;
+
+    function addCandidates(string memory _name) public onlyOwner {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(_name, 0);
+    }
+
+    function Seecandidatedetail(uint _id) public view returns (string memory name) {
+        Candidate memory candidate = candidates[_id];
+        return candidate.name;
+    }
+
+    function vote(uint _idofCandidate) public hasVoted { 
+        alreadyVoted[msg.sender] = true;
+        Candidate storage candidate = candidates[_idofCandidate];   
+        candidate.voteCount++;
+    }
+
+    function seewhoWon() public view returns (string memory winnerName, uint winnerVoteCount) {
+        uint winningVoteCount = 0;
+        uint winningCandidateId = 0;
+
+        for (uint i = 1; i <= candidatesCount; i++) {
+            if (candidates[i].voteCount > winningVoteCount) {
+                winningVoteCount = candidates[i].voteCount;
+                winningCandidateId = i;
+            }
+        }
+
+        if (winningCandidateId == 0) {
+            return ("No candidates", 0);
+        }
+
+        return (candidates[winningCandidateId].name, candidates[winningCandidateId].voteCount);
+    }
+}
